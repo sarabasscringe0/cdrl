a04be93faf89acc61497eb6cc6020b2e5af55897
sara <saraespedal8@gmail.com>
2026-02-14 22:24:36 +0100

    Update cdrl.c
    finished parser, cant say its optimised lmao

--- cdrl.c
+++ cdrl.c
@@ -3,20 +3,12 @@
 #include <stdlib.h>
 
 char (*pars(char *x, char delimiter))[256] {
-    for (int i = 0; x[i] != '\0'; i++){ // remove newlines
-        if (x[i]=='\n') {
-            for (int j = i; x[j] != '\0'; j++) {
-                x[j] = x[j+1];
-            }
-            i--;
-        } 
-    }
-    // count delimiters
     int nest = 0;
     int quotes = 0;
     int escaped = 0;
     int count = 0;
-    for (int i=0; x[i] != '\0'; i++) {
+    int countplusremovenewlines(int whichone) {
+        for (int i=0; x[i] != '\0'; i++) {
         if (x[i] == '(' || x[i] == '[' || x[i] == '{') { // nest ++ if left bracket
             nest++;
         }
@@ -24,12 +16,25 @@ char (*pars(char *x, char delimiter))[256] {
             nest--;
         }
         if (!escaped && x[i] == '"') {quotes = !quotes;} // track quotes
-        if (x[i] == delimiter && nest == 0 && !quotes && !escaped) {
-            count++;
+        if (whichone) {
+            if (x[i]=='\n' && !escaped && !quotes) { // remove newlines
+                for (int j = i; x[j] != '\0'; j++) {
+                    x[j] = x[j+1];
+                }
+                i--;
+            } 
+        } else {
+            if (x[i] == delimiter && nest == 0 && !quotes && !escaped) { // count delimiters
+                count++;
+            }
         }
         if (escaped) {escaped = 0;} // unescape, important location after escape uses and before escape assignment
         if (x[i] == '\\') {escaped = 1;} // escape
+        }
     }
+    countplusremovenewlines(1);
+    
+    countplusremovenewlines(0);
     //prepare for parsing
     nest = 0;
     quotes = 0;
@@ -52,7 +57,7 @@ char (*pars(char *x, char delimiter))[256] {
             stacklen = 0;
             stack[0] = '\0';
         }
-        if (x[i] != '\\' || escaped) {
+        if (x[i] != delimiter && x[i] != '\\' || escaped) {
             // append character to stack
             stack[stacklen++] = x[i];
             stack[stacklen] = '\0';
@@ -64,9 +69,9 @@ char (*pars(char *x, char delimiter))[256] {
 }
 
 int main() {
-    char str[] = "test.helloworld;\nhi im \nsometh\\\";\\\"ing i\";\" think;o\nk;";
+    char str[] = "test.helloworld;\nhi im \\\nsom\"\n\"eth\\\";\\\"ing i\";\" think;o\nk;";
     char (*ls)[256] = pars(str,';');
-    printf("%s\n", ls[0]);
+    printf("%s\n", ls[6]);
     free(ls);
     return 0;
 }
